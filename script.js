fcc41fa655f599766771325faac91e08a9b830d8
Genoxus <rusell2delprado@gmail.com>
2021-08-05 23:45:50 +0800

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const burgerIcon = document.getElementById('burger-icon');
+const burger = document.getElementById('burger');
+
+// Get value of label and input
+// add Action listener for label
+// if checkbox is check
+
+burger.addEventListener('click', display);
+
+
+
